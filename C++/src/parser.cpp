c5abfcca0189a93b3edf6e31e96e9d4979349bc7
Cerek Hillen <crockeo@gmail.com>
2014-12-27 00:59:42 -0500

    I think completed the interpreter?

--- C++/src/parser.cpp
+++ C++/src/parser.cpp
@@ -2,6 +2,7 @@
 
 //////////////
 // Includes //
+#include <iostream>
 #include <istream>
 
 #include "brainfuck.hpp"
@@ -9,7 +10,62 @@
 //////////
 // Code //
 
+// Attempting to start a location.
+BrainfuckState doLoop(BrainfuckState state, std::string str, int& loc) {
+    int eloc;
+    for (eloc = loc; str[eloc] != ']'; eloc++) {
+        if (eloc >= str.size())
+            throw BrainfuckException("Expected a ']' to close the loop.\n");
+    }
+
+    std::string sub = str.substr(loc + 1, eloc - 1);
+    while (state.peek())
+        state = runParser(state, sub);
+    loc = eloc + 1;
+
+    return state;
+}
+
+// Parsing a single character in the brainfucks.
+BrainfuckState parseChar(BrainfuckState state, char c) {
+    switch (c) {
+    case '>':
+        state.loc++;
+        break;
+    case '<':
+        state.loc--;
+        break;
+    case '+':
+        state.poke(state.peek() + 1);
+        break;
+    case '-':
+        state.poke(state.peek() - 1);
+        break;
+    case '.':
+        std::cout << state.peek();
+        break;
+    case ',':
+        std::cout << "NEED TO IMPLEMENT ','!!!" << std::endl;
+        break;
+    case '[':
+        throw BrainfuckException("Unexpected '['!\n");
+    case ']':
+        throw BrainfuckException("Unexpected ']'!\n");
+    default:
+        throw BrainfuckException("Unexpected character!\n");
+    }
+
+    return state;
+}
+
 // Running a parser on an input and a BrainfuckState.
 BrainfuckState runParser(BrainfuckState state, std::string str) throw(BrainfuckException) {
+    for (int loc = 0; loc < str.size(); loc++) {
+        if (str[loc] == '[')
+            state = doLoop(state, str, loc);
+        else
+            state = parseChar(state, str[loc]);
+    }
+
     return state;
 }
