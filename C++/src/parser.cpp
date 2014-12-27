5ede43648e8d556a67d074179e3958e89999d03b
Cerek Hillen <crockeo@gmail.com>
2014-12-26 23:07:17 -0500

    Started doing the parser thing.

---
+++ C++/src/parser.cpp
@@ -0,0 +1,15 @@
+#include "parser.hpp"
+
+//////////////
+// Includes //
+#include <istream>
+
+#include "brainfuck.hpp"
+
+//////////
+// Code //
+
+// Running a parser on an input and a BrainfuckState.
+BrainfuckState runParser(BrainfuckState state, std::istream& in) {
+    return state;
+}
---
+++ C++/src/parser.hpp
@@ -0,0 +1,16 @@
+#ifndef _PARSER_HPP_
+#define _PARSER_HPP_
+
+//////////////
+// Includes //
+#include <istream>
+
+#include "brainfuck.hpp"
+
+//////////
+// Code //
+
+// Running a parser on an input and a BrainfuckState.
+BrainfuckState runParser(BrainfuckState, std::istream&);
+
+#endif
